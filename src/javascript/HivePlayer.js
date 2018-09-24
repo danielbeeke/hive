687c400aaa9ad73c554a2b71fa7fcffc8edc418b
Daniël Beeke <d.beeke@oxilion.nl>
2018-09-24 19:37:49 +0200

    shadow is clean

--- src/javascript/HivePlayer.js
+++ src/javascript/HivePlayer.js
@@ -6,9 +6,9 @@ customElements.define('hive-player', class HivePlayer extends HTMLElement {
    * If this HTML is loaded from a clean state we add all the pieces.
    */
   connectedCallback () {
-    if (!this.children.length) {
-      let player = this.getAttribute('player');
+    let player = this.getAttribute('player');
 
+    if (!this.children.length) {
       this.innerHTML = `
         <hive-queen player="${player}"></hive-queen>
   
@@ -34,7 +34,9 @@ customElements.define('hive-player', class HivePlayer extends HTMLElement {
       `;
     }
 
-    new ShadowScroll(this);
+    new ShadowScroll(this, {
+      prefix: 'shadow-' + player
+    });
 
   }
 });
--- src/javascript/ShadowScroll.js
+++ src/javascript/ShadowScroll.js
@@ -36,7 +36,7 @@
   SingleShadowScroll = (function() {
 
     SingleShadowScroll.prototype.options = {
-      prefix: 'shadowScroll',
+      prefix: 'shadow',
       noStyles: false
     };
 
@@ -45,52 +45,10 @@
       extend(this.options, options);
       this.cont = element;
       this.elements = {
-        top: document.createElement('div'),
-        bottom: document.createElement('div'),
         left: document.createElement('div'),
         right: document.createElement('div'),
       };
-      this.defaultStyles = {
-        top: {
-          width: '100%',
-          height: '4px',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          background: 'linear-gradient(to bottom, rgba(0,0,0,0.05), rgba(0,0,0,0))',
-          '-webkit-transition': 'opacity 0.2s'
-        },
-        bottom: {
-          width: '100%',
-          height: '4px',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          background: 'linear-gradient(to top, rgba(0,0,0,0.05), rgba(0,0,0,0))',
-          '-webkit-transition': 'opacity 0.2s'
-        },
-        left: {
-          width: '4px',
-          height: '100%',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          background: 'linear-gradient(to right, rgba(0,0,0,0.05), rgba(0,0,0,0))',
-          '-webkit-transition': 'opacity 0.2s'
-        },
-        right: {
-          width: '4px',
-          height: '100%',
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          background: 'linear-gradient(to left, rgba(0,0,0,0.05), rgba(0,0,0,0))',
-          '-webkit-transition': 'opacity 0.2s'
-        }
 
-      };
-      this.createElement(this.cont, 'top');
-      this.createElement(this.cont, 'bottom');
       this.createElement(this.cont, 'left');
       this.createElement(this.cont, 'right');
 
@@ -102,33 +60,17 @@
     }
 
     SingleShadowScroll.prototype.createElement = function(context, element) {
-      this.elements[element].setAttribute('class', "" + this.options.prefix + "_" + element);
-      this.elements[element] = context.appendChild(this.elements[element]);
-      if (!this.options.noStyles) {
-        return setStyles(this.elements[element], this.defaultStyles[element]);
-      }
+      this.elements[element].setAttribute('class', "" + this.options.prefix + " shadow-" + element);
+      this.elements[element] = context.parentNode.appendChild(this.elements[element]);
     };
 
     SingleShadowScroll.prototype.scroll = function() {
-      var clientHeight, scrollHeight, scrollTop, shadowHeight;
-      scrollTop = this.cont.scrollTop;
-      clientHeight = this.cont.clientHeight;
-      scrollHeight = this.cont.scrollHeight;
-      shadowHeight = this.elements.top.offsetHeight;
-      this.elements.top.style.top = "" + scrollTop + "px";
-      this.elements.bottom.style.top = "" + (scrollTop + clientHeight - shadowHeight) + "px";
-      this.elements.top.style.opacity = scrollTop;
-      this.elements.bottom.style.opacity = scrollTop + clientHeight >= scrollHeight ? 0 : 1;
-
-      var clientWidth, scrollWidth, scrollLeft, shadowWidth;
+      var clientWidth, scrollWidth, scrollLeft;
       scrollLeft = this.cont.scrollLeft;
       clientWidth = this.cont.clientWidth;
       scrollWidth = this.cont.scrollWidth;
-      shadowWidth = this.elements.top.offsetWidth;
-      this.elements.left.style.left = "" + scrollLeft + "px";
-      this.elements.right.style.left = "" + (scrollLeft + clientWidth - shadowWidth) + "px";
-      this.elements.left.style.opacity = scrollLeft;
-      this.elements.right.style.opacity = scrollLeft + clientWidth >= scrollWidth ? 0 : 1;
+      this.elements.left.style.opacity = scrollLeft ? .3 : 0;
+      this.elements.right.style.opacity = scrollLeft + clientWidth >= scrollWidth ? 0 : .3;
     };
 
     return SingleShadowScroll;
